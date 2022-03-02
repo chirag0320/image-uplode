e779d95b0a81cbb6513b12346892a89a97ea5c2d
chirag0320 <namchiraggohil@gmail.com>
2022-03-02 19:18:10 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+// import CardHeader from "@mui/material/CardHeader";
+// import CardMedia from "@mui/material/CardMedia";
+// import CardContent from "@mui/material/CardContent";
+// import CardActions from "@mui/material/CardActions";
+// import Collapse from "@mui/material/Collapse";
+// import Avatar from "@mui/material/Avatar";
+// import IconButton from "@mui/material/IconButton";
+// import Typography from "@mui/material/Typography";
+// import { red } from "@mui/material/colors";
+// import FavoriteIcon from "@mui/icons-material/Favorite";
+// import ShareIcon from "@mui/icons-material/Share";
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+// import MoreVertIcon from "@mui/icons-material/MoreVert";
+// import userim from "./ima.jfif";
+import { useState } from "react";
+import Map from "react-map-gl";
+// import "./.env";
 function App() {
+  const [state, setstate] = useState({
+    longitude: -100,
+    latitude: 40,
+    zoom: 10,
+    height: "100px",
+    width: "100px",
+  });
+  // const Map = ReactMapboxGl({
+  //   accessToken:
+  //     "pk.eyJ1IjoiY2hpcmFnMTIxMjEyIiwiYSI6ImNsMDlnc2NicjBkM3gzY3A5d3NsdmFsZnYifQ.QdHK17UlgDkfyQMgWsbtKg",
+  // });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {console.log("yess")}
+      <Map
+        initialViewState={{
+          longitude: -100,
+          latitude: 40,
+          zoom: 3.5,
+        }}
+        mapboxAccessToken="pk.eyJ1IjoiY2hpcmFnMTIxMjEyIiwiYSI6ImNsMDlnc2NicjBkM3gzY3A5d3NsdmFsZnYifQ.QdHK17UlgDkfyQMgWsbtKg"
+        // mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        style={{ width: 600, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
+      />
+      chirag
+    </>
   );
 }
 
